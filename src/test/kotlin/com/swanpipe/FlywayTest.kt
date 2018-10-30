862041a18334b78b056489b58b32fdcb9009ce2a
andy <andy@arin.net>
2018-10-30 11:18:13 -0700

    now testing flyway migration against postgres!!!!!

---
+++ src/test/kotlin/com/swanpipe/FlywayTest.kt
@@ -0,0 +1,19 @@
+package com.swanpipe
+
+import com.opentable.db.postgres.embedded.EmbeddedPostgres
+import org.junit.jupiter.api.Test
+import org.flywaydb.core.Flyway
+
+
+
+class FlywayTest {
+
+    @Test
+    fun testFlyway() {
+        val pg = EmbeddedPostgres.start()
+        println( "Embedded Postgres started on port ${pg!!.getPort()}" )
+        val flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:${pg.port}/postgres", "postgres", "secret").load()
+        flyway.migrate()
+    }
+
+}
